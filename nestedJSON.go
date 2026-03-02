f53537ef0fe52e86d017f0abb6391465b7ab98be
bintangcloud <bintangkelaci@gmail.com>
2026-03-02 16:41:43 +0800

    marshal final

--- nestedJSON.go
+++ nestedJSON.go
@@ -5,13 +5,36 @@ import (
 	"fmt"
 )
 
+type Customer struct {
+	Name  string `json:"name"`
+	Email string `json:"email"`
+}
+
+type Items struct {
+	ProductName string `json:"product_name"`
+	Price       int    `json:"price"`
+	Quantity    int    `json:"quantity"`
+}
+
 type Order struct {
-	Customers string `json:"customers"`
-	Items     string `json:"items`
+	ID       int      `json:"id"`
+	Customer Customer `json:"customer"`
+	Items    []Items  `json:"items"`
 }
 
 func main() {
-	o1 := Order{Customers: "Bintang", Items: "Buku"}
-	jsonData, _ := json.Marshal(o1)
+	p1 := Order{
+		ID: 101,
+		Customer: Customer{
+			Name:  "Bintang",
+			Email: "bintang@gmail.com",
+		},
+		Items: []Items{
+			{ProductName: "Novel", Price: 200000, Quantity: 1},
+			{ProductName: "Pencil", Price: 2000, Quantity: 2},
+		},
+	}
+
+	jsonData, _ := json.Marshal(p1)
 	fmt.Println("Hasil JSON:", string(jsonData))
 }
